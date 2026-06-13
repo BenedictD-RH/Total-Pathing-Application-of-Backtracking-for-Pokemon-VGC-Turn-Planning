6677813070a5e7d292a1a4b6a93daf4caa57fc20
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-06-13 15:07:22 +0700

    chore: initialized repo

---
+++ src/main/java/stima/App.java
@@ -0,0 +1,16 @@
+package stima;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(App.class, new String[0]);
+    }
+}
